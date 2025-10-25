2a55f9e9c182b4ae1de405720e1f4bb9bdcbb27f
DouglasSerbino <dserbino@gmail.com>
2025-10-24 19:26:50 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 import './src/env.mjs';
 /** @type {import('next').NextConfig} */
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const nextConfig = {
   images: {
     remotePatterns: [
